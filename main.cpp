f021f60d6c3050ece7ca9aa34d5066a055ecc1f9
Jeevan Bains <harjeevanbains@gmail.com>
2018-10-31 20:16:29 -0700

    final project imported from a working local project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,90 @@
-#include <iostream>
+#include<iostream>
+#include <memory.h>
+#include <fstream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+set<string> dictionary;
+ofstream out_data_short("TopWordsShort.txt");
+ofstream out_data_final("FinalWords.txt");
+
+void setDictionary() {
+    ifstream inFile;
+    inFile.open("dictionary.txt");
+    string dicWord;
+    while (inFile.good()) {
+        inFile >> dicWord;
+        dictionary.insert(dicWord);
+    }
+}
+
+bool check(string word){
+    return (dictionary.find(word) != dictionary.end());
+}
+
+bool contains(vector<string> words, const string b) {
+    bool within;
+    for (int i = 0; i < words.size() - 1; i++) {
+        if (words[i] == b) {
+            within = true;
+            return within;
+        }
+    }
+    within = false;
+    return within;
+}
+
+
+void generate1(string word, int start, int length,int size) {
+    int i;
+    if (start == length) {
+        string temp = word;
+        int big = temp.length() - size;
+        temp.erase(temp.end() - big, temp.end());
+        if (check(temp)) {
+            out_data_short << temp << endl;
+        }
+    } else {
+        for (i = start; i <= length; i++) {
+            swap((word[start]), (word[i]));
+            generate1(word, start + 1, length,size);
+            swap((word[start]), (word[i])); //backtrack
+        }
+    }
+}
+
+void shrink() {
+    ifstream inFile;
+    inFile.open("TopWordsShort.txt");
+    set<string> words;
+    set<string>::iterator wordsIT;
+    string currentWord;
+    while (inFile >> currentWord) {
+        string temp = currentWord;
+        if (words.find(temp) == words.end()) {
+            words.insert(temp);
+        }
+    }
+    for(wordsIT=words.begin(); wordsIT!=words.end();++wordsIT) {
+        cout<<(*wordsIT)<<endl;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    setDictionary();
+    string word;
+    int size;
+    cout<<"Enter the letters:"<<endl;
+    cin>>word;
+    cout<<"Enter the size of the wanted word:"<<endl;
+    cin>>size;
+    int length = word.length();
+    generate1(word, 0, length - 1,size);
+    cout<<"Possible words: "<<endl;
+    shrink();
+    cout<<""<<endl;
+    cout << "DONE" << endl;
     return 0;
 }
